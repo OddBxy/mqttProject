3de68a8ec7415a411fc0fd4cdd4f03a0a9bc7a09
Pierre <pierregeiguer@gmail.com>
2025-04-29 15:31:59 +0200

    Ehhh la ca marche

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MQTTCommunicationService } from './mqtt.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: false,
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: false
 })
-export class AppComponent {
-  title = 'mqttProject';
+export class AppComponent implements OnInit {
+  message = '';
+  messages: string[] = [];
+
+  constructor(private mqttService: MQTTCommunicationService) {}
+
+  ngOnInit(): void {
+    this.mqttService.observeTopic('test/topic').subscribe((msg) => {
+      this.messages.push(msg.payload.toString());
+    });
+  }
+
+  send(): void {
+    if (this.message.trim()) {
+      this.mqttService.publish('test/topic', this.message);
+      this.message = '';
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'broker.hivemq.com',  // Exemple de broker public
+  port: 8000,                      // WebSocket port
+  path: '/mqtt',                  // souvent '/mqtt' pour WS
+  protocol: 'ws'
+};
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
+
+
